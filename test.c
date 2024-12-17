a72cf4f0fc401b89e62c6bab77f596450bf1fdce
BhargaviBuelah <47322171+BhargaviBuelah@users.noreply.github.com>
2024-12-17 21:03:53 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,14 @@
+/*hellooo */
+#include<stdio.h>
+int main()
+{
+    float num1 =987 ;
+    char ch = 'a';
+    char str[] = "helloWorld";
+    switch(ch)
+    {
+        case 1:
+    }
+    
+     printf ("hello\n");
+}
